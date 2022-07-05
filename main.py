cb9b6470453984ccad6a1cdc3b58398442514b06
aiymm <71624576+aiymm@users.noreply.github.com>
2022-07-05 14:05:36 +0600

    metrics added

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import math
 import string
 from typing import Optional
+import prometheus_client
 
 from fastapi import FastAPI, Request, HTTPException, Header
 from pydantic import BaseModel
+from prometheus_client import Counter, Histogram, Gauge
+
 
 app = FastAPI()
+metrics_app = prometheus_client.make_asgi_app()
+app.mount("/metrics", metrics_app)
 
 class Item(BaseModel):
     element: str
+
+c = Counter('http_requests_total', 'Number of HTTP requests received')
+h = Histogram('http_requests_milliseconds', 'Duration of HTTP requests in milliseconds')
+gsum = Gauge('last_sum1n', 'Value stores last result of sum1n')
+gfibo = Gauge('last_fibo', 'Value stores last result of fibo')
+gsize = Gauge('list_size', 'Value stores current list size')
+gcalc = Gauge('last_calculator', 'Value stores last result of calculator')
+ccalc = Counter('errors_calculator_total','Number of errors in calculator')
 
 
 @app.get("/")
@@ -19,8 +32,11 @@ def read_root():
 @app.get("/sum1n/{number}")
 async def sum_to_n(number: int):
     sum = 0
+    c.inc()
+    h.observe(0.2)
     for i in range(1,number+1):
         sum = sum + i
+    gsum.set(sum)
     return {"result": sum}
 
 #fibo
@@ -30,14 +46,17 @@ async def fibo(n: int):
     n1 = 1
 
     if n == 0:
+        gfibo.set(0)
         return {"result": 0}
     elif n == 1:
+        gfibo.set(1)
         return {"result": 1}
     else:
         for i in range (2, n):
             ans = n0 + n1
             n0 = n1
             n1 = ans
+        gfibo.set(ans)
         return {"result": ans}
 
 #reverse
@@ -55,6 +74,7 @@ async def update_list(item: Item):
 
 @app.get("/list")
 async def get_list():
+    gsize.set(len(elements))
     return {"result": elements}
 
 @app.get("/math/{num}")
@@ -85,7 +105,7 @@ async def calculator(calculator: str):
     num1 = int(arr[0])
     num2 = int(arr[2])
     op = arr[1] # arithmetic operation
-    result = -1
+    result = "-1"
     if op == '+':
         result = num1 + num2
     elif op == '-':
@@ -93,10 +113,15 @@ async def calculator(calculator: str):
     elif op == '*':
         result = num1 * num2
     elif op == '/':
-        result = int(num1 / num2)
+        if num2 == 0:
+            ccalc.inc()
+            result = 0
+        else:
+            result = int(num1 / num2)
     else:
        result = 0
     
+    gcalc.set(result)
     return {"result": result}
     
 
